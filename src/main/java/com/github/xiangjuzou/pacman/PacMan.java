3bc5e2559bd295ca47d44297513542d590217e5a
xiangju-zou <xiangju.zou@gmail.com>
2026-04-06 21:48:34 +0200

    werkende hello world met maven en yaeger dependency

--- src/main/java/com/github/xiangjuzou/pacman/PacMan.java
+++ src/main/java/com/github/xiangjuzou/pacman/PacMan.java
@@ -3,7 +3,7 @@ package com.github.xiangjuzou.pacman;
 public class PacMan {
 
     public static void main(String[] args) {
-        System.out.println("hallo wereld");
+        System.out.println("hallo wereld!");
 
     }
 }
